09b9d7da9cf63796fc1957026628a0685bd75715
Adoem <99o713@naver.com>
2024-12-23 20:03:26 +0900

    Map 불러옴, Nav2안됨, launch파일 정리

---
+++ build/carter_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/adoem/nav2_test/src/carter_test
+# Build directory: /home/adoem/nav2_test/build/carter_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
